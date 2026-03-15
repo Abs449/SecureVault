093c9f231ff8333dce3da71c172011658c744ff9
Abs449 <cabhiram2506@gmail.com>
2026-03-16 01:24:28 +0530

    security change

--- extension/config.js
+++
@@ -1,8 +0,0 @@
-window.firebaseConfig = {
-  "apiKey": "AIzaSyCHBeEV24zkxXn1B9kL3bhtQhndU-1wGKQ",
-  "authDomain": "passwordmanager-42b9a.firebaseapp.com",
-  "projectId": "passwordmanager-42b9a",
-  "storageBucket": "passwordmanager-42b9a.firebasestorage.app",
-  "messagingSenderId": "928283070192",
-  "appId": "1:928283070192:web:3948dbd69d0dabe229fc35"
-};
